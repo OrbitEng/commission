1f86d208d9c51fb049594da3d9da8b1d61780693
indianeros58 <0xDEADBEAT@gmail.com>
2022-10-01 19:07:08 -0400

    frgt summ

--- programs/orbit-commission/src/accessors/commission_tx_common.rs
+++ programs/orbit-commission/src/accessors/commission_tx_common.rs
@@ -64,8 +64,19 @@ pub struct CloseTransactionAccount<'info>{
     pub wallet: Signer<'info>,
 
     #[account(
+        address = commission_transaction.metadata.buyer,
+        seeds = [
+            b"orbit_account",
+            buyer_wallet.key().as_ref()
+        ],
+        bump,
+        seeds::program = market_accounts::ID
+    )]
+    pub buyer_account: Account<'info, OrbitMarketAccount>,
+
+    #[account(
         mut,
-        address = market_account.wallet
+        address = buyer_account.wallet
     )]
     pub buyer_wallet: SystemAccount<'info>
 }
@@ -315,7 +326,7 @@ impl<'a, 'b, 'c, 'd, 'e, 'f, 'g> OrbitTransactionTrait<'a, 'b, 'c, 'd, 'e, 'f, '
     }
 
     fn close_transaction_account(ctx: Context<CloseTransactionAccount>) -> Result<()>{
-        ctx.accounts.commission_transaction.close(ctx.accounts.market_account.to_account_info())
+        ctx.accounts.commission_transaction.close(ctx.accounts.buyer_account.to_account_info())
     }
 }
 
