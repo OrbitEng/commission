88ac9df8089d9d8a640695f0836add3d6ec468f8
0xDEADBEAT <indianerios58@gmail.com>
2023-03-23 10:42:26 -0400

    ?

--- tests/orbit-commission.ts
+++ tests/orbit-commission.ts
@@ -1,5 +1,5 @@
-import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
 import { OrbitCommission } from "../target/types/orbit_commission";
 
 describe("orbit-commission", () => {
